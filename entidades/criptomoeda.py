f58f803e06fec892c405fb84afa23d21e8a14a38
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-05-07 20:38:55 -0300

    first commit

---
+++ entidades/criptomoeda.py
@@ -0,0 +1,26 @@
+class Criptomoeda:
+    def __init__(self,
+                 id: str,
+                 symbol: str,
+                 name: str,
+                 image: str,
+                 current_price: float,
+                 market_cap: float,
+                 market_cap_rank: int,
+                 fully_diluted_valuation: int,
+                 total_volume: int,
+                 high_24h: float,
+                 low_24h: float,
+                 price_change_24h: float,
+                 price_change_percentage_24h: float,
+                 market_cap_change_24h: float,
+                 market_cap_change_percentage_24h: float,
+                 circulating_supply: float,
+                 total_supply: float,
+                 max_supply: float,
+                 ath: float,
+                 ath_change_percentage: float,
+                 ath_date
+
+                 ) -> None:
+        pass
