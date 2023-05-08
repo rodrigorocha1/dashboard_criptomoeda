d5671a7d32efda68396702333b94fc705b755d04
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-05-07 21:23:30 -0300

    docstring

---
+++ entidades/transacoes.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+
+
+class Transacao:
+    """Classe que representa uma transacao
+
+    """
+
+    @staticmethod
+    def _converter_timestamp(timestamp: int) -> str:
+        """Converte timestamp para hora no formato dd-mm-AAAA
+
+        Args:
+            timestamp (int): timestamp
+
+        Returns:
+            str: data e hora Ex: 10-08-2020 20:00:00
+        """
+
+        timestamp = timestamp // 1000
+        data = datetime.fromtimestamp(timestamp)
+        data_str_formato = '%d-%m-%Y %H:%M:%S'
+        data = data.strftime(data_str_formato)
+        return data
+
+    def __init__(self, preco: float, timestamp: int) -> None:
+        """Construtor da classe 
+
+        Args:
+            preco (float): preço da criptomoeda
+            timestamp (int): timestamp da transacao
+        """
+
+        self._preco = preco
+        self._timestamp = self._converter_timestamp(timestamp)
+
+    @property
+    def timestamp(self) -> str:
+        """retorna o valor sem modificar
+
+        Returns:
+            str: data e hora Ex: 10-08-2020 20:00:00 
+        """
+        return self._timestamp
