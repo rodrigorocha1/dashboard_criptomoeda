9843e35085793d9095d59301aa190aedcaf90213
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-05-08 21:44:02 -0300

    doc string

--- service/transacao_service.py
+++ service/transacao_service.py
@@ -9,8 +9,17 @@ class TransacaoService:
         self._url = 'https://api.coingecko.com/api/v3/coins'
 
     def obter_transacoes(self, id_cripto: str, days: int, interval: str):
+        """Obtem as transações da criptomoeda
 
+        Args:
+            id_cripto (str): id Ex bitcoin
+            days (int): intervalo de dias 2
+            interval (str): days, month, year
+
+        Returns:
+            _type_: json de preços
+        """
         url = f'{self._url}/{id_cripto}/market_chart?vs_currency=brl&days={days}&interval={interval}'
         if self._ping.testar_conexao() == 200:
-            req = requests.get(url)
-            return req.json()
+            prices = requests.get(url)
+            return prices.json()['prices']
